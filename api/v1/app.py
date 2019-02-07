fcc92a0839ea10ac57508f38e960f988b381521b
TrieToSucceed <jian.huang.8581@gmail.com>
2019-02-07 00:43:59 +0000

    add prettyprint module to print json in a readable fashion

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
